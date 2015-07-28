4c9cb9ad9c303f65566865ecec510f4edc35150a
draconian00 <korea_syh@naver.com>
2015-07-28 19:00:04 +0900

    Update arithgeo.rb

--- arithgeo.rb
+++ arithgeo.rb
@@ -3,7 +3,7 @@ def ArithGeo(arr)
   for i in (1...arr.length-1) do
     if arr[i] - arr[i-1] == arr[i+1] - arr[i]
       output = "Arithmetic"
-    elsif arr[i] / arr[i-1] == arr[i+1] - arr[i]
+    elsif arr[i] / arr[i-1] == arr[i+1] / arr[i]
       ouput = "Geometric"
     else
       output = -1
