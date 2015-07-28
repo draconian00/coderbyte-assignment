0afcee159592f2cd06d9be6ed286540a54ea0290
draconian00 <korea_syh@naver.com>
2015-07-28 15:38:15 +0900

    Create checknums.rb

---
+++ checknums.rb
@@ -0,0 +1,14 @@
+def CheckNums(num1,num2)
+
+  index = -1
+  result = true
+  if num1 > num2
+    result = false
+    return result
+  elsif num1 < num2
+    return result
+  else
+    return index
+  end
+         
+end
